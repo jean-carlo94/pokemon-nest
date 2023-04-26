e95c3fe1f535193f932632679073631f06207898
Jean Carlo Urrego <jctaziturno@gmail.com>
2023-04-26 11:58:30 -0500

    pagination

--- README.md
+++ README.md
@@ -29,4 +29,12 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
-5. Semilla
+5. Reconstruir la base de datos con la semilla
+```
+htpp://localhost:3000/api/v2/seed
+```
+
+# Stack Usado
+
+1. MondoDB
+2. NestJS
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,22 +1,49 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter,
+  ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=1',
+    await this.pokemonModel.deleteMany({});
+
+    const data = await this.http.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=650',
     );
 
+    const pokemonToInsert: { name: string; no: number }[] = data.results.map(
+      (pokeRes) => ({ name: pokeRes.name, no: +pokeRes.url.split('/')[6] }),
+    );
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+    return 'Seed Execute';
+  }
+  /* Método de Inserción Multiple
+  async executeSeed() {
+    await this.pokemonModel.deleteMany({});
+
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+    const insertPromiseArray = [];
     data.results.forEach(({ name, url }) => {
       const segments = url.split('/');
       const no: number = +segments[segments.length - 2];
-
-      console.log({ name, no });
+      //const pokemon = await this.pokemonModel.create({ name, no });
+      insertPromiseArray.push(this.pokemonModel.create({ name, no }));
     });
-    return data.results;
+
+    await Promise.all(insertPromiseArray);
+    return 'Seed Executed';
   }
+  */
 }
